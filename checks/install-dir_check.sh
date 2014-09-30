7c3a673a93b208afcbcbaed3d4c4b680274c54d8
David Spencer <baildon.research@googlemail.com>
2014-09-30 02:09:17 +0100

    Add new checks.
    install-dir_check.sh
    ownership_check.sh
    symlink_check.sh
    tar113_check.sh
    
    Signed-off-by: David Spencer <baildon.research@googlemail.com>

---
+++ checks/install-dir_check.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Copyright 2014  Heinz Wiesinger, Amsterdam, The Netherlands
+# Copyright 2014  David Spencer, Baildon, West Yorkshire, U.K.
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Verify that the 'install' directory exists.
+
+check() {
+  if [ ! -d "$WORKING_DIR/install" ]; then
+    log_error "no-install-dir"
+  fi
+}
+
+info() {
+  if [ "$1" = "no-install-dir" ]; then
+    echo -n "The file does not contain an install/ directory. It is probably "
+    echo "not a Slackware package."
+    echo
+  fi
+}
---
+++ checks/symlink_check.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Copyright 2014  Heinz Wiesinger, Amsterdam, The Netherlands
+# Copyright 2014  David Spencer, Baildon, West Yorkshire, U.K.
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Verify that there are no symlinks in the package.
+
+check() {
+  find "$WORKING_DIR" -type l | while read symlink; do
+    log_warning "package-contains-symlink" "$symlink"
+  done
+}
+
+info() {
+  if [ "$1" = "package-contains-symlink" ]; then
+    echo "Symbolic link found. These should normally be removed by makepkg."
+    echo
+  fi
+}
