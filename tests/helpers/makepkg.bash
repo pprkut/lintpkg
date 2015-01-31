eddc340e35a03fc5b627f308fce76e4f0a730fed
Heinz Wiesinger <pprkut@liwjatan.at>
2015-01-31 15:06:57 +0100

    Add test helper methods to create tar listings.

--- tests/helpers/makepkg.bash
+++ tests/helpers/makepkg.bash
@@ -41,5 +41,60 @@ create_slackware_package() {
   return 0
 }
 
+create_tar_listing() {
+  BASE="$1"
+  NEW="$2"
 
+  cd $BASE
+    while read line && ! [ -z "$line" ]; do
+
+      if [ -z "$NEW" -a "$(echo $line | wc -L)" -gt 2 ]; then
+        line=$(echo "$line" | sed 's|./||')
+      fi
+
+      if ! [ -z "$NEW" ] && [ "$line" = "./" ]; then
+        continue
+      fi
+
+      echo "$line"
+    done <<< "$(find . -exec ls -dp {} \;)"
+
+  cd - &> /dev/null
+  return 0
+}
+
+create_detailed_tar_listing() {
+  BASE="$1"
+  NEW="$2"
+
+  cd $BASE
+    while read line && ! [ -z "$line" ]; do
+      TYPE=$(echo $line | head -c 1)
+      PERMS=$(echo $line | cut -d " " -f 1)
+      OWNER=$(echo $line | cut -d " " -f 3)
+      GROUP=$(echo $line | cut -d " " -f 4)
+      DATE=$(echo $line | cut -d " " -f 6)
+      TIME=$(echo $line | cut -d " " -f 7)
+      FILE=$(echo $line | cut -d " " -f 8-)
+
+      if [ -z "$NEW" -a "$(echo $FILE | wc -L)" -gt 2 ]; then
+        FILE=$(echo "$FILE" | sed 's|./||')
+      fi
+
+      if ! [ -z "$NEW" ] && [ "$FILE" = "./" ]; then
+        continue
+      fi
+
+      if [ "$TYPE" = "d" -o "$TYPE" = "l" ]; then
+        SIZE=0
+      else
+        SIZE=$(echo $line | cut -d " " -f 5)
+      fi
+
+      echo "$PERMS $OWNER/$GROUP $SIZE $DATE $TIME $FILE"
+    done <<< "$(find . -exec ls -dlp --time-style=long-iso {} \;)"
+
+  cd - &> /dev/null
+  return 0
+}
 
